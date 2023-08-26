19108a797fcce20e0de66675463480eb72aadbcb
iwaderem <iwaderem@ncsu.edu>
2023-08-26 07:05:19 -0400

    Fixed file name issues

--- riddle-app/src/App.js
+++ riddle-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   // Single riddle properties
   const question = "On top an oval green with scattered paths, stands a quiet place where stories lay";
   const answer = 'thompson library';
-  const audioSrc = `${process.env.PUBLIC_URL}/audio/thompson.m4a`;
+  const audioSrc = `${process.env.PUBLIC_URL}/audio/Thompson.m4a`;
 
   return (
     <div className="App">
