4c0670d56b47bd9343db07d8e2f54033a5d049d9
Ifeoluwa Aderemi <58048118+Benediximus@users.noreply.github.com>
2024-08-24 10:35:28 -0400

    Added mission transcript

--- riddle-app/src/AudioPlayer.js
+++ riddle-app/src/AudioPlayer.js
@@ -6,12 +6,13 @@ function AudioPlayer({ audioSrc }) {
   return (
     <div>
       <h2>Here's the mission, listen carefully</h2>
+      <p>Example</p>
       <ReactAudioPlayer
         src={audioSrc}
         autoPlay
         controls
       />
-      <h2>The next QR code is located near the entrance of the library on the statue side!</h2>
+      <h2>Shhhhhhh... this mission requires a secret agent to infiltrate the building and take a recon photo of the rest of their team from the ___ floor inside the building. Make sure to be very very quiet. Your team should be standing near the entracne of the buildling on the stature side. Give us your best post</h2>
     </div>
   );
 }
